311b573e208a4dc25cea9d6b1a4ba448e028b774
PriyankaBose <babui.priyanka@gmail.com>
2015-03-16 04:12:08 +0530

    test

--- CodeChef/Experiment/test.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int a[100000000]={ [ 0 ... 9999999 ] = 1 };
-int main()
-{
-	return 0;
-}
--- CodeChef/test3.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-	int n1,n2,n3,n4;
-	
-	return 0;
-}
